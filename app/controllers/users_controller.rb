eda24e29c831460f2f8994f3cdf4c4b5d44cffc0
Julia Dziuba <julia@juliadziuba.com>
2013-04-22 14:27:13 -0700

    Finish user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to Geronimo! Add more information about yourself to fill out your website or add works, clients and venues to start building your database!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Julia Dziuba"
+    email    "julia@example.com"
+    password "danceParty"
+    password_confirmation "danceParty"
+  end
+end
