7713f84144bcde352b7eceef736ddcdfab334dfa
Julia Dziuba <julia@juliadziuba.com>
2013-05-04 07:27:16 -0700

    Correct worksubcategory and worksubtype controller files

---
+++ app/controllers/worksubcategories_controller.rb
@@ -0,0 +1,43 @@
+class WorksubcategoriesController < ApplicationController
+  before_filter :signed_in_user
+
+  def show
+    @workcategory = current_user.workcategories.find(params[:workcategory_id])
+    @worksubcategory = current_user.worksubcategories.find(params[:id])
+    @works = @worksubcategory.works
+  end
+
+  def new
+    @workcategory = current_user.workcategories.find(params[:workcategory_id])
+    @worksubcategory = Worksubcategory.new
+  end
+
+  def create
+    @workcategory = current_user.workcategories.find(params[:workcategory_id])
+    @worksubcategory = @workcategory.worksubcategories.build(params[:worksubcategory])
+    if @worksubcategory.save
+      redirect_to workcategory_url(@workcategory)
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @workcategory = current_user.workcategories.find(params[:workcategory_id])
+    @worksubcategory = @workcategory.worksubcategories.find_by_id(params[:id])
+  end
+
+  def update
+     @workcategory = current_user.workcategories.find(params[:workcategory_id])
+    if @workcategory.worksubcategories.find_by_id(params[:id]).update_attributes(params[:worksubcategory])
+      redirect_to workcategories_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    current_user.worksubcategories.find_by_id(params[:id]).destroy
+    redirect_to workcategories_path
+  end
+end
