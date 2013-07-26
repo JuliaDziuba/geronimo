6b2763c3842bf62fef85c7bec875161f93b06ae1
Julia Dziuba <julia@juliadziuba.com>
2013-07-25 21:22:18 -0700

    Remove constants

--- config/initializers/s3_constants.rb
+++
@@ -1,7 +0,0 @@
-# config/initializers/s3_constants.rb
-
-if Rails.env.development?
-	AWS_BUCKET = "makersmoon"
-  	AWS_ACCESS_KEY_ID = "AKIAI5L6S2KSTEAAUYKA"
-  	AWS_SECRET_ACCESS_KEY = "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"
-end
