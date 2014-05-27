43796fef49a095f64120159587244e12b8a8de84
Julia Dziuba <julia@juliadziuba.com>
2014-05-27 14:22:06 -0700

    Removing mail chimp api.

--- config/initializers/gibbon.rb
+++ config/initializers/gibbon.rb
@@ -1,5 +1,3 @@
-# config/initializers/s3_constants.rb
-
-Gibbon::API.api_key = "c5eecab33527fd9cc144242419b9dc33-us7"
+# config/initializers/gibbon.rb
 Gibbon::API.timeout = 15
 Gibbon::API.throws_exceptions = false
